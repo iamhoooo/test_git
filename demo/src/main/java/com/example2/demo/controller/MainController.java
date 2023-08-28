925430d7eea6df8ab06d249a8e7f1064dd0cd969
hoo <tkdgn9098@gmail.com>
2023-08-28 16:45:39 +0900

    first commit

---
+++ demo/src/main/java/com/example2/demo/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.example2.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // RestController 어노테이션을 통해 MainController클래스가 Controller 역할을 하는 것임을 설정한다. RestController의 주용도는 Json형태로 객체 데이터를 반환 하는것.
+public class MainController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!",name);
+
+    }
+}
