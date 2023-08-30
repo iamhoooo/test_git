036c010ecce024e6953a65507ff806f851d2b85e
hoo <tkdgn9098@gmail.com>
2023-08-30 15:07:37 +0900

    템플릿-main 추가 및 MainController 수정

--- demo/src/main/java/com/example2/demo/controller/MainController.java
+++ demo/src/main/java/com/example2/demo/controller/MainController.java
@@ -1,11 +1,13 @@
 package com.example2.demo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController // RestController 어노테이션을 통해 MainController클래스가 Controller 역할을 하는 것임을 설정한다. RestController의 주용도는 Json형태로 객체 데이터를 반환 하는것.
+//@RestController // RestController 어노테이션을 통해 MainController클래스가 Controller 역할을 하는 것임을 설정한다. RestController의 주용도는 Json형태로 객체 데이터를 반환 하는것.
+@Controller
 public class MainController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String hello(@RequestParam(value = "name", defaultValue = "AI") String name) {
